70c0e810f67c75eccaeeeb5781fe27ccce877568
공수한 <ksh92315@pionnet.co.kr>
2025-03-25 23:40:33 +0900

    앱 라우터의 데이터 페칭

--- app/(with-searchbar)/page.jsx
+++ app/(with-searchbar)/page.jsx
@@ -1,21 +1,53 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+
+//모든 도서
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다....</div>;
+  }
+
+  const allBooks = await response.json();
+  return (
+    <>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </>
+  );
+}
+//추천 도서
+async function RecoBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다....</div>;
+  }
+  const recoBooks = await response.json();
+
+  return (
+    <>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- app/(with-searchbar)/search/page.jsx
+++ app/(with-searchbar)/search/page.jsx
@@ -1,12 +1,20 @@
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 
-export default function Page({ searchParams }) {
+export default async function Page({ searchParams }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생하였습니다...</div>;
+  }
+
+  const searchBooks = await response.json();
+
   return (
     <div>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
+      {searchBooks &&
+        searchBooks.map((book) => <BookItem key={book.id} {...book} />)}
     </div>
   );
 }
