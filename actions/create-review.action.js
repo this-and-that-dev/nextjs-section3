65541dccbbe36f0e5af98b1a2a307e8f84ef279e
공수한 <ksh92315@pionnet.co.kr>
2025-04-08 23:05:59 +0900

    useActionState 를 활용한 더블클릭, disabled 처리, 에러 처리

--- actions/create-review.action.js
+++ actions/create-review.action.js
@@ -1,29 +1,35 @@
 "use server";
 import { revalidatePath, revalidateTag } from "next/cache";
+import { delay } from "@/util/delay";
 
-async function createReviewAction(formData) {
+async function createReviewAction(_, formData) {
   const bookId = formData.get("bookId").toString();
   const content = formData.get("content").toString();
   const author = formData.get("author").toString();
 
   if (!content || !author) {
-    return;
+    return { status: false, error: "리뷰 내용과 작성자를 입력해주세요." };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review1`,
       {
         method: "POST",
         body: JSON.stringify({ content, author, bookId }),
       },
     );
-    console.log(response.status);
-    // revalidatePath(`/book/${bookId}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return { status: false, error: `리뷰 저장에 실패했습니다. : ${err}` };
   }
 }
 
